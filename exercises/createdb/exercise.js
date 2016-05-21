395b0350f7b0d09ef39c09782c3e3c3043b444e0
Ricardo Malta <ricardo@cloudoki.com>
2016-05-21 15:00:37 +0100

    create db

--- exercises/createdb/exercise.js
+++ exercises/createdb/exercise.js
@@ -1,1 +1,38 @@
-const Exercise = require('workshopper-exercise'); module.exports = new Exercise()
+var exercise = require('workshopper-exercise')();
+var r = require('rethinkdb');
+var connection;
+
+exercise.requireSubmission = false;
+
+
+
+exercise.addSetup(function(mode, callback){
+
+    r.connect({ host: "localhost" }, function(err, conn) {
+        if( err )
+            throw err;
+
+        connection = conn;
+
+        process.nextTick(callback);
+    });
+});
+
+exercise.addProcessor(function(mode, callback) {
+
+    var pass = false;
+
+    r.dbList().run(connection, function(err, res){
+
+        if( err ){
+            this.emit("fail", err);
+            connection.close();
+        }
+
+        pass = res.indexOf('toolbox') > -1;
+
+        return callback(null, pass);
+    });
+});
+
+module.exports = exercise;
