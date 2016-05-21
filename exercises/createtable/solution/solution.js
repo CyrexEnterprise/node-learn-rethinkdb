d58df30a4e29a2ced20449232148a9709d55a370
Ricardo Malta <ricardo@cloudoki.com>
2016-05-21 15:17:34 +0100

    install md, createtable

--- exercises/createtable/solution/solution.js
+++ exercises/createtable/solution/solution.js
@@ -1,1 +1,18 @@
 // solution stuff here
+
+var r = require('rethinkdb');
+
+r.connect({ host: "localhost" }, function(err, conn) {
+    if( err )
+        throw err;
+
+    r.db('toolbox').tableCreate('screws').run(conn, function(err, res){
+        if( err )
+            console.error("Error:",err);
+
+        conn.close(function(err) {
+                if (err)
+                    throw err;
+            });
+    });
+});
--- exercises/install/problem.md
+++ exercises/install/problem.md
@@ -1,4 +1,12 @@
-Install RethinkDB
+# Introduction
+
+RethinkDB is a NoSQL db engine with the promise to solve a lots of real time web issues. It gives you out of the box this cool features:
+
+- Pure JSON documents
+- Natural query language (ReQL)
+- Table joins
+- Document change feeds (can be used as )
+- Nice atomic operations
 
 -----------------------------------------------------------
 ## HINTS
